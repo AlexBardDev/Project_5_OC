fc218b0f3b0bbf65fccfa070b0c3739e77038214
AlexBardDev <alexandre.bardiaux@outlook.com>
2018-03-27 10:32:57 +0200

    Refactoring feature 3

--- functions.py
+++ functions.py
@@ -1,9 +1,12 @@
-#Local library
-from import_data_from_the_API_to_the_database.create_database import db, Category, FoodSubstituted
+"""
+This script contains 5 functions. 4 of them are used by 'pur_beurre.py'. But
+the first one is used in this script to check if the inouts are valid or not.
+"""
 
 def valid_input(data_input):
-    """This functions checks if the input is valid or not."""
-    
+    """This function checks if the input is valid or not. It takes the input
+    and it returns a boolean."""
+
     is_valid = False
     try:
         data_input = int(data_input)
@@ -14,14 +17,22 @@ def valid_input(data_input):
 
     return is_valid
 
-def select(list_objects, message):
-    """pass"""
+def user_selection(list_objects, message):
+    """This function helps the user to make a choice. It displays all the
+    possibilities and returns the user's choice. It takes a list of objects
+    and a message to display. It returns the object selected by the user."""
 
     while True:
+
+        #Display the possibilities
         print(message)
         for i, elt in enumerate(list_objects):
             print("""{} - {}""".format(i+1, elt.name))
+
+        #The user enters an input
         choice = input("""Votre choix : """)
+
+        #Check that the input is correct. If not, it restarts the loop
         if valid_input(choice) == True:
             if int(choice) in range(1, len(list_objects)+1):
                 break
@@ -32,29 +43,41 @@ def select(list_objects, message):
 
 def display_food(food):
     """This function displays all the information saved in the database about
-    the selected food."""
+    the selected product. It takes 1 parameter : an object coming from the
+    database."""
 
     print("""Voici le résultat de votre sélection :""")
-    print("""Aliment à substituer : {}. Substitut plus sain : {}.""".format(food.name, food.substituted_product_name))
+    print("""Aliment à substituer : {}. Substitut plus sain : {}."""
+          .format(food.name, food.substituted_product_name))
 
     if food.description != "":
         print("""Voici une description plus complète du substitut : {}""".format(food.description))
-    
-    if food.stores != "":
-        print("""Vous pouvez trouvez ce produit dans le(s) magasin(s) suivant(s) : {}""".format(food.stores))
 
-    print("""Pour plus d'informations, voici la page complète du substitut : {}""".format(food.link))
+    if food.stores != "":
+        print("""Vous pouvez trouvez ce produit dans le(s) magasin(s) suivant(s) : {}"""
+              .format(food.stores))
+
+    print("""Pour plus d'informations, voici la page complète du substitut : {}"""
+          .format(food.link))
 
 def save_substitute(food):
-    """pass"""
+    """This function check if the substitute is saved in the bookmarks or not.
+    If not, it gives the possibility to save it. It takes 1 parameter : a
+    'food' object coming from the database. It retuns a boolean."""
 
     change = False
 
+    #Is already saved in the bookmarks
     if food.is_saved == True:
         print("""Ce substitut est déjà enregistré dans vos favoris.""")
+    #Is not already saved
     else:
         while True:
-            choice = input("""Ce substitut n'est pas encore enregistré dans vos favoris.\nVoulez-vous l'enregistrer (=tapez 1) ou non (=tapez 2) ? """)
+            #The user entres something
+            choice = input("""Ce substitut n'est pas encore enregistré dans vos favoris.
+                           \nVoulez-vous l'enregistrer (=tapez 1) ou non (=tapez 2) ? """)
+
+            #Check if the input is valid
             if valid_input(choice) == True:
                 if choice == "1":
                     print("""Ce subsitut est maintenant dans vos favoris.""")
@@ -69,10 +92,15 @@ def save_substitute(food):
     return change
 
 def finish():
-    """pass"""
+    """This function takes no parameters and returns a boolean. With this
+    function, we know if the user wants to leave the program or continue."""
 
     while True:
-        choice = input("""Voulez-vous revenir à l'écran d'accueil (=tapez 1)\nou quittez le programme (=tapez 2) ? """)
+        #The users enters the choice
+        choice = input("""Voulez-vous revenir à l'écran d'accueil (=tapez 1)
+                       \nou quittez le programme (=tapez 2) ? """)
+
+        #Check that the input is valid
         if valid_input(choice) == True:
             if choice == "1":
                 response = False
@@ -84,7 +112,3 @@ def finish():
                 print("""Vous n'avez pas entré un nombre correct. Veuillez recommencer...""")
 
     return response
-
-
-
-#Mettre le texte dans les input    
--- pur_beurre.py
+++ pur_beurre.py
@@ -6,52 +6,71 @@ program.
 #Standard library
 import time
 
-#External library
-
 #Local library
 from import_data_from_the_API_to_the_database.create_database import db, Category, FoodSubstituted
-from functions import *
+from functions import user_selection, display_food, save_substitute, finish
 
 print("""Bonjour cher(e) utilisateur(trice) ! Que puis-je faire pour vous aujourd'hui...""")
 
+#The loop of the program
 ACTIVE = True
-while ACTIVE :
+while ACTIVE:
+
     print("""1 - Quel aliment souhaitez-vous remplacer ?\n2 - Retrouver mes aliments substitués.""")
     choice = input()
 
+    #If the user wants to find a new substitute
     if choice == "1":
+        #Get all the categories from the database
         with db:
             categories = Category.select()
 
-        selected_category = select(categories, "Catégories :")
+        #The user selects a category
+        selected_category = user_selection(categories, "Catégories :")
 
+        #Get all products related to the right category
         with db:
-            list_food = FoodSubstituted.select().join(Category).where(Category.id == selected_category.id)
+            list_food = FoodSubstituted.select().join(Category).where(Category.id ==
+                                                                      selected_category.id)
 
-        selected_food = select(list_food, """Aliments de la categorie '{}'' :""".format(selected_category.name))
+        #The user selects food
+        selected_food = user_selection(list_food,
+                                       """Aliments de la categorie '{}'' :""".format(selected_category.name))
 
+        #Display the data
         display_food(selected_food)
 
+        #Save the product in the bookmarks if necessary
         if save_substitute(selected_food) == True:
             with db:
-                modified_food = FoodSubstituted.update(is_saved=True).where(FoodSubstituted.id == selected_food.id)
-                modified_food.execute()
+                modify_food = FoodSubstituted.update(is_saved=True).where(FoodSubstituted.id ==
+                                                                          selected_food.id)
+                modify_food.execute()
 
+        #Finish the program or restart at the begin
         if finish() == True:
             ACTIVE = False
 
+    #If the user wants to see all already subsituted products
     elif choice == "2":
+        #Get all bookmarks
         with db:
             list_saved_sub = FoodSubstituted.select().where(FoodSubstituted.is_saved == True)
 
-        selected_sub = select(list_saved_sub, "Vos aliments favoris sont :")
+        #The user selects the bookmark that he wants
+        selected_sub = user_selection(list_saved_sub, "Vos aliments favoris sont :")
 
+        #Display th data
         display_food(selected_sub)
 
+        #Finish the program or restart at the begin
         if finish() == True:
             ACTIVE = False
-    else:
-        print("""Oops, vous n'avez pas entré une commande valide. Veuillez recommencer...""")      
 
+    #If the input is not valid
+    else:
+        print("""Oops, votre commande n'est valide. Veuillez recommencer...""")
+
+#The end of the program
 print("""Au revoir cher(e) utilisateur(trice) ! A bientôt !""")
 time.sleep(2)
